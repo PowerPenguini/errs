92ed88152d4a80e89fe3a5d661e8647ff0f14d24
Mateusz Jaśkiewicz <jaskiewiczteo@gmail.com>
2026-03-30 17:29:56 +0200

    Add generic unauthorized and forbidden errors

--- generic.go
+++ generic.go
@@ -3,3 +3,5 @@ package errs
 var GenericInternal = NewError("internal_server_error", "Internal server error", InternalType, nil)
 var GenericInvalidJson = NewError("invalid_payload", "Invalid JSON payload", BadRequestType, nil)
 var GenericInvalidQuery = NewError("invalid_query", "At least one query argument is invalid", BadRequestType, nil)
+var GenericUnauthorized = NewError("unauthorized", "Authentication required", UnauthorizedType, nil)
+var GenericForbidden = NewError("forbidden", "Insufficient permissions", ForbiddenType, nil)
